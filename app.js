55f64e01e2d517c49968526cff1ec423c9161bd0
Jose Lopez <chema.lopezp@gmail.com>
2017-09-16 16:25:08 -0700

    express server

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require('fs');
+var http = require('http');
+
+var app = express();
+
+app.use('/', express.static('public/'));
+
+/**
+ * Create HTTP server.
+ */
+
+app.listen(8080, function () {
+  console.log('Express server listening on port 8080');
+});
